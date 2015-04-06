2705b5ed71cdd81708063466a052dcb0910b48f7
tommasobratto <tbratto22@gmail.com>
2015-04-06 16:07:59 +0100

    added edit feature

--- spec/post_features_spec.rb
+++ spec/post_features_spec.rb
@@ -41,9 +41,26 @@ Then did he raise on high the Holy Hand Grenade of Antioch, saying, "Bless this,
       visit '/posts'
       click_link 'Holy Hand Grenade of Antioch'
       expect(page).to have_content 'Holy Hand Grenade of Antioch'
-      expect(page).to have_content 'A Reading from the Book of Armaments, Chapter 4, Verses 16 to 20: 
-Then did he raise on high the Holy Hand Grenade of Antioch, saying, "Bless this, O Lord, that with it thou mayst blow thine enemies to tiny bits, in thy mercy." And the people did rejoice and did feast upon the lambs and toads and tree-sloths and fruit-bats and orangutans and breakfast cereals ... Now did the Lord say, "First thou pullest the Holy Pin. Then thou must count to three. Three shall be the number of the counting and the number of the counting shall be three. Four shalt thou not count, neither shalt thou count two, excepting that thou then proceedeth to three. Five is right out. Once the number three, being the number of the counting, be reached, then lobbest thou the Holy Hand Grenade in the direction of thine foe, who, being naughty in my sight, shall snuff it."'
+      expect(page).to have_content 'A Reading from the Book of Armaments, Chapter 4, Verses 16 to 20'
       expect(current_path).to eq "/posts/#{hhgoa.id}"
     end
-  end 
+  end
+
+  context 'editing posts' do 
+    before do 
+      Post.create(title: 'Holy Hand Grenade', description: 'A Reading from the Book of Armaments, Chapter 5')
+    end
+
+    scenario 'let a user edit a posts' do
+      visit '/posts'
+      click_link 'Edit Holy Hand Grenade'
+      fill_in 'Title', with: 'Holy Hand Grenade of Antioch'
+      fill_in 'Description', with: 'A Reading from the Book of Armaments, Chapter 4'
+      click_button 'Update Post'
+      expect(page).to have_content 'Holy Hand Grenade of Antioch'
+      expect(current_path).to eq "/posts"
+      click_link 'Holy Hand Grenade of Antioch'
+      expect(page).to have_content 'A Reading from the Book of Armaments, Chapter 4'
+    end
+  end
 end

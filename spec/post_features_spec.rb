a9f9f0f87bf2848d9ea1a55ae54cf4406b0a3e46
tommasobratto <tbratto22@gmail.com>
2015-04-06 14:05:20 +0100

    added post model, view and controller

--- spec/post_features_spec.rb
+++ spec/post_features_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-describe Post do 
+feature 'instagram posts' do 
   
+  context 'no posts added' do 
+    scenario 'should display "add picture" to the page' do 
+      visit '/posts'
+      expect(page).to have_content 'no posts added'
+      expect(page).to have_link 'submit a post'
+    end
+  end
+
+  context 'post added' do 
+    before do 
+      Post.create(title: 'Holy Hand Grenade of Antioch')
+    end
+
+    scenario 'display post' do 
+      visit '/posts'
+      expect(page).not_to have_content 'no posts added'
+      expect(page).to have_content 'Holy Hand Grenade of Antioch'
+    end
+  end
+
 end
